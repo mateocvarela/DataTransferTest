11f9aa221d849baae3eccd9304a29770502b8548
mateocastrillonv <mateo.castrillon@juju.com.co>
2023-11-01 11:39:12 -0500

    all files upload to db and handle exeption for employee cases

--- DB_UPLOAD.py
+++ DB_UPLOAD.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyodbc
 import csv
+import numpy as np
 from sqlalchemy import create_engine
 
 # RDS database connection settings
@@ -10,8 +11,12 @@ db_port='1433'
 username = 'master'
 password = 'master123'
 
+# List of CSV files to process
+csv_files = ['departments.csv','jobs.csv','hired_employees.csv']
+
+
 # Specify the path to your local CSV file
-df = pd.read_csv('C:/Users/arqinfraestructura/Documents/Code_Challenge/Downloads/departments.csv',header=None, names=['id', 'departments'])
+#df = pd.read_csv('C:/Users/arqinfraestructura/Documents/Code_Challenge/Downloads/departments.csv',header=None, names=['id', 'departments'])
 connection_string = (
     f'DRIVER={{ODBC Driver 17 for SQL Server}};'
     f'SERVER={server};'
@@ -20,19 +25,33 @@ connection_string = (
     f'PWD={password};'
 )
 connection = pyodbc.connect(connection_string)
-print(df)
+
+table_names = ['departments','jobs','employes' ]
 # Generate the SQL query to insert data into the table
-insert_query = f'INSERT INTO departmentstest (id, departments) VALUES (?, ?)'
+for csv_file, table_name in zip(csv_files, table_names):
+    df = pd.read_csv(f'C:/Users/arqinfraestructura/Documents/Code_Challenge/Downloads/{csv_file}', header=None)
+    if table_name == 'employes':
+        df.iloc[:, -2:] = df.iloc[:, -2:].replace([np.inf, -np.inf, np.nan], 0)
+        # Convert the last two columns (index -2 and -1) to integers
+        df.iloc[:, -2:] = df.iloc[:, -2:].apply(np.int64) 
+                # Handle empty values by replacing them with an empty string for the name column
+        df.iloc[:, 1] = df.iloc[:, 1].replace([np.nan], ['not named'])
+        
+        # Handle empty values by replacing them with a specific date or datetime for the date column
+        df.iloc[:, 2] = df.iloc[:, 2].replace([np.nan], ['2022-01-01T00:00:00Z'])
+     
+    placeholders = ', '.join(['?'] * len(df.columns))
+    insert_query = f'INSERT INTO {table_name} VALUES ({placeholders})'
 
 # Create a cursor to execute the SQL query
-cursor = connection.cursor()
+    cursor = connection.cursor()
 
 # Loop through the DataFrame and insert each row into the SQL Server table
-for index, row in df.iterrows():
-    cursor.execute(insert_query, row['id'], row['departments'])
+    for _, row in df.iterrows():
+        cursor.execute(insert_query, *row)
 
 # Commit the changes to the database
-connection.commit()
+    connection.commit()
 
 # Close the database connection
 connection.close()
