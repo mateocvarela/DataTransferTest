5ee11a51f33efe03895e154bb0341fb40d40256e
mateocastrillonv <mateo.castrillon@juju.com.co>
2023-11-10 09:25:16 -0500

    add line to local test

--- DB_UPLOAD.py
+++ DB_UPLOAD.py
@@ -37,7 +37,7 @@ for csv_file, table_name in zip(csv_files, table_names):
     csv_body = csv_obj['Body']
     csv_string = csv_body.read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_string), header=None)
-    #df = pd.read_csv(f'C:/Users/arqinfraestructura/Documents/Code_Challenge/Downloads/{csv_file}', header=None)
+    #df = pd.read_csv(f'C:/Users/arqinfraestructura/Documents/Code_Challenge/Downloads/{csv_file}', header=None) #line for local testing
     if table_name == 'employes':
         df.iloc[:, -2:] = df.iloc[:, -2:].replace([np.inf, -np.inf, np.nan], 0)
         # Convert the last two columns (index -2 and -1) to integers
